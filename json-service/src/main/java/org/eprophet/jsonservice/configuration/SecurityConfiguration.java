9328c280cb51a0e468e4001a32c5250fd60955f5
Dharmaonline <34622931+Dharmaonline@users.noreply.github.com>
2017-12-17 18:11:54 +0530

    initial commit

---
+++ json-service/src/main/java/org/eprophet/jsonservice/configuration/SecurityConfiguration.java
@@ -0,0 +1,12 @@
+package org.eprophet.jsonservice.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+//@Configuration
+public class SecurityConfiguration {
+
+    
+
+}
